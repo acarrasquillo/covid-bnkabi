1e11282b909d0bf0f778038d2e1c1e8ef74ab0f1
Abimael Carrasquillo <abimael.carrasquillo@nielsen.com>
2021-04-21 02:50:15 -0700

    adding some simple graph

--- main.py
+++ main.py
@@ -3,6 +3,14 @@
 import pandas as pd
 import os
 from sodapy import Socrata
+import numpy as np
+import matplotlib
+import cufflinks as cf
+import plotly
+import plotly.offline as py
+import plotly.graph_objs as go
+cf.go_offline()
+py.init_notebook_mode()
 
 # Unauthenticated client only works with public data sets. Note 'None'
 # in place of application token, and no username or password:
@@ -13,9 +21,9 @@ client = Socrata("data.cdc.gov", os.environ["SOCRATA_APP_TOKEN"])
 
 # First 2000 results, returned as JSON from API / converted to Python list of
 # dictionaries by sodapy.
-results = client.get("n8mc-b4w4", limit=2000)
+results = client.get("n8mc-b4w4", case_month="2020-02")
 
 # Convert to pandas DataFrame
 results_df = pd.DataFrame.from_records(results)
 
-print(results_df)
+results_df.value_counts(subset=["res_state"]).plot(kind='bar')
